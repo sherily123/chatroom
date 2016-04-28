363324cb27951366e888fdb4de8cf7073f5345bc
Sherry Zeng <sherily123@ricequant.com>
2016-04-28 18:18:33 +0800

    update html & css

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // 引入必需模块
 var express = require('express'),
     app = express(),
-    server = require('http').createServer(app),
-    sassMiddleware = require('node-sass-middleware');
+    server = require('http').createServer(app);
 
 // 指定静态文件路径
 app.use(express.static(__dirname + '/public'));
